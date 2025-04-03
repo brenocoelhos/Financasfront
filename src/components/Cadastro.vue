ce6e625f165ae0f785c6dbcf074efcfa4f4aac86
AdrianResende <92228901+AdrianResende@users.noreply.github.com>
2025-04-03 11:21:21 -0300

    fix: add router to login

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -1,74 +1,25 @@
 <template>
-  <v-container
-    class="d-flex justify-center align-center no-border"
-    style="height: 100vh; background-color: #f5f5f5"
-  >
-    <v-card
-      class="pa-5"
-      elevation="10"
-      style="max-width: 400px; width: 100%; border-radius: 12px"
-    >
-      <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2"
-        >AppFinanças</v-card-title
-      >
+  <v-container class="d-flex justify-center align-center no-border" style="height: 100vh; background-color: #f5f5f5">
+    <v-card class="pa-5" elevation="10" style="max-width: 400px; width: 100%; border-radius: 12px">
+      <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2">AppFinanças</v-card-title>
       <v-divider class="mb-4"></v-divider>
-      <v-text-field
-        v-model="usuario"
-        label="Usuário"
-        variant="outlined"
-        dense
-        class="mb-3"
-      />
-      <v-text-field
-        v-model="senha"
-        label="Senha"
-        variant="outlined"
-        dense
-        type="password"
-        class="mb-4"
-      />
-      <v-text-field
-        v-model="confirmarSenha"
-        label="Confirmar Senha"
-        variant="outlined"
-        dense
-        type="password"
-        class="mb-4"
-      />
-    <div class="d-flex justify-space-between">
-            <v-btn
-            variant="outlined"
-            color="primary"
-            class="ma-2"
-            style="flex: 1"
-            @click="handleRegister"
-            >
-            Cadastrar
-            </v-btn>
-            <v-btn
-            variant="text"
-            color="secondary"
-            class="ma-2"
-            style="flex: 1"
-            @click="$router.push('/')"
-            >
-            Voltar
-            </v-btn>
-            </div>
+      <v-text-field v-model="usuario" label="Usuário" variant="outlined" dense class="mb-3" />
+      <v-text-field v-model="senha" label="Senha" variant="outlined" dense type="password" class="mb-4" />
+      <v-text-field v-model="confirmarSenha" label="Confirmar Senha" variant="outlined" dense type="password"
+        class="mb-4" />
+      <div class="d-flex justify-space-between">
+        <v-btn variant="outlined" color="primary" class="ma-2" style="flex: 1" @click="handleRegister">
+          Cadastrar
+        </v-btn>
+        <v-btn variant="text" color="secondary" class="ma-2" style="flex: 1" @click="$router.push('/login')">
+          Voltar
+        </v-btn>
+      </div>
     </v-card>
   </v-container>
-  <v-alert
-    v-model="alert"
-    type="error"
-    dismissible
-    class="mt-4"
-    :style="{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }"
-    closable
-    :border="false"
-    :color="'red'"
-    :icon="'mdi-alert-circle'"
-    :elevation="2"
-  >
+  <v-alert v-model="alert" type="error" dismissible class="mt-4"
+    :style="{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }" closable
+    :border="false" :color="'red'" :icon="'mdi-alert-circle'" :elevation="2">
     {{ alertMessage }}
   </v-alert>
 </template>
