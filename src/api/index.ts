f59cf125ee2d58062fd4a7806d4202fb3f7f906a
AdrianResende <92228901+AdrianResende@users.noreply.github.com>
2025-04-03 19:50:02 -0300

    fix: to url base to backend

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6060', // URL base do backend
+  baseURL: 'http://localhost:8080', // URL base do backend
   timeout: 5000, // Tempo limite para requisições
 });
 
