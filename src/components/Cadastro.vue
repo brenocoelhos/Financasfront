74dfb7add0c57e850751ea9266e3c1dc4125f2c3
AdrianResende <adrianresende147@gmail.com>
2025-04-09 14:37:06 -0300

    fix: add v-alert-sucess and fix send router

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -1,28 +1,36 @@
 <template>
   <v-card elevation="0" style="border-radius: 12px; background-color: #f5f5f5">
-  <v-container class="d-flex justify-center align-center no-border" style="height: 100vh; background-color: #f5f5f5">
-    <v-card class="pa-5" elevation="10" style="max-width: 400px; width: 100%; border-radius: 12px">
-      <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2">AppFinanças</v-card-title>
-      <v-divider class="mb-4"></v-divider>
-      <v-text-field v-model="usuario" label="Usuário" variant="outlined" dense class="mb-3" />
-      <v-text-field v-model="senha" label="Senha" variant="outlined" dense type="password" class="mb-4" />
-      <v-text-field v-model="confirmarSenha" label="Confirmar Senha" variant="outlined" dense type="password"
-        class="mb-4" />
-      <div class="d-flex justify-space-between">
-        <v-btn variant="outlined" color="primary" class="ma-2" style="flex: 1" @click="handleRegister">
-          Cadastrar
-        </v-btn>
-        <v-btn variant="text" color="secondary" class="ma-2" style="flex: 1" @click="$router.push('/login')">
-          Voltar
-        </v-btn>
-      </div>
-    </v-card>
-  </v-container>
-  <v-alert v-model="alert" type="error" dismissible class="mt-4"
-    :style="{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }" closable
-    :border="false" :color="'red'" :icon="'mdi-alert-circle'" :elevation="2">
-    {{ alertMessage }}
-  </v-alert>
+    <v-container class="d-flex justify-center align-center no-border" style="height: 100vh; background-color: #f5f5f5">
+      <v-card class="pa-5" elevation="10" style="max-width: 400px; width: 100%; border-radius: 12px">
+        <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2">AppFinanças</v-card-title>
+        <v-divider class="mb-4"></v-divider>
+        <v-text-field v-model="name" label="Name" variant="outlined" dense class="mb-3" />
+        <v-text-field v-model="email" label="Usuário" variant="outlined" dense class="mb-3" />
+        <v-text-field v-model="password" label="password" variant="outlined" dense type="password" class="mb-4" />
+        <v-text-field v-model="confirmarpassword" label="Confirmar password" variant="outlined" dense type="password"
+          class="mb-4" />
+        <div class="d-flex justify-space-between">
+          <v-btn variant="outlined" color="primary" class="ma-2" style="flex: 1" @click="handleRegister">
+            Cadastrar
+          </v-btn>
+          <v-btn variant="text" color="secondary" class="ma-2" style="flex: 1" @click="$router.push('/login')">
+            Voltar
+          </v-btn>
+        </div>
+      </v-card>
+    </v-container>
+    <v-alert
+      v-model="alert"
+      :type="alertType"
+      dismissible
+      class="mt-4"
+      :style="{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }"
+      :color="alertType === 'success' ? 'green' : 'red'"
+      :icon="alertType === 'success' ? 'mdi-check-circle' : 'mdi-alert-circle'"
+      :elevation="2"
+    >
+      {{ alertMessage }}
+    </v-alert>
   </v-card>
 </template>
 
@@ -32,36 +40,41 @@ import { useRouter } from 'vue-router';
 import { register } from '../api/auth';
 
 const router = useRouter();
-const usuario = ref<string>('');
-const senha = ref<string>('');
-const confirmarSenha = ref<string>('');
+const name = ref<string>('');
+const email = ref<string>('');
+const password = ref<string>('');
+const confirmarpassword = ref<string>('');
 const alert = ref<boolean>(false);
 const alertMessage = ref<string>('');
+const alertType = ref<'error' | 'success' | 'info' | 'warning' | undefined>('error');
 
 const handleRegister = async () => {
   const validations = [
-    { condition: usuario.value === '', message: 'Por favor, preencha o usuário.' },
-    { condition: senha.value === '', message: 'Por favor, preencha a senha.' },
-    { condition: confirmarSenha.value === '', message: 'Por favor, confirme a senha.' },
-    { condition: senha.value !== confirmarSenha.value, message: 'As senhas não coincidem.' },
+    { condition: email.value === '', message: 'Por favor, preencha o email.' },
+    { condition: password.value === '', message: 'Por favor, preencha a password.' },
+    { condition: confirmarpassword.value === '', message: 'Por favor, confirme a password.' },
+    { condition: password.value !== confirmarpassword.value, message: 'As passwords não coincidem.' },
   ];
 
   const failedValidation = validations.find(validation => validation.condition);
   if (failedValidation) {
     alertMessage.value = failedValidation.message;
+    alertType.value = 'error';
     showAlert();
     return;
   }
 
   try {
-    await register(usuario.value, senha.value);
+    await register(name.value, email.value, password.value);
     alertMessage.value = 'Cadastro realizado com sucesso!';
+    alertType.value = 'success';
     showAlert();
     setTimeout(() => {
       router.push('/login');
-    }, 3000);
+    });
   } catch (error) {
     alertMessage.value = String(error);
+    alertType.value = 'error'; // Define o tipo como erro
     showAlert();
   }
 };
@@ -73,7 +86,6 @@ const showAlert = () => {
   }, 3000);
 };
 </script>
-
 
 <style scoped>
 .no-border {
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,25 @@
 <template>
   <v-card elevation="0" style="border-radius: 12px; background-color: #f5f5f5">
-    <v-container class="d-flex justify-center align-center no-border" style="height: 100vh; background-color: #f5f5f5">
-      <v-card class="pa-5" elevation="10" style="max-width: 400px; width: 100%; border-radius: 12px">
-        <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2">AppFinanças</v-card-title>
+    <v-container
+      class="d-flex justify-center align-center no-border"
+      style="height: 100vh; background-color: #f5f5f5"
+    >
+      <v-card
+        class="pa-5"
+        elevation="10"
+        style="max-width: 400px; width: 100%; border-radius: 12px"
+      >
+        <v-card-title class="text-h5 text-center mb-4" style="color: #1976d2"
+          >AppFinanças</v-card-title
+        >
         <v-divider class="mb-4"></v-divider>
-        <v-text-field v-model="usuario" label="Usuário" variant="outlined" dense class="mb-3" />
+        <v-text-field
+          v-model="usuario"
+          label="Usuário"
+          variant="outlined"
+          dense
+          class="mb-3"
+        />
         <v-text-field
           v-model="senha"
           :type="showPassword ? 'text' : 'password'"
@@ -16,57 +31,108 @@
           @click:append-inner="togglePasswordVisibility"
         />
         <div class="d-flex justify-space-between">
-          <v-btn variant="outlined" color="primary" class="ma-2" style="flex: 1" @click="handleLogin">
+          <v-btn
+            variant="outlined"
+            color="primary"
+            class="ma-2"
+            style="flex: 1"
+            @click="handleLogin"
+            :disabled="loading"
+          >
             Entrar
           </v-btn>
-          <v-btn variant="text" color="secondary" class="ma-2" style="flex: 1" @click="$router.push('/cadastro')">
+          <v-btn
+            variant="text"
+            color="secondary"
+            class="ma-2"
+            style="flex: 1"
+            @click="$router.push('/cadastro')"
+          >
             Cadastrar
           </v-btn>
         </div>
       </v-card>
     </v-container>
-    <v-alert v-model="alert" type="error" dismissible class="mt-4"
-      :style="{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }" closable
-      :border="false" :color="'red'" :icon="'mdi-alert-circle'" :elevation="2">
+
+    <v-overlay :value="loading" absolute>
+      <v-skeleton-loader
+        indeterminate
+        size="64"
+        color="primary"
+      ></v-skeleton-loader>
+    </v-overlay>
+
+    <v-alert
+      v-model="alert"
+      :type="alertType"
+      dismissible
+      class="mt-4"
+      :style="{
+        position: 'fixed',
+        top: '20px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        zIndex: 1000,
+      }"
+      :color="alertType === 'success' ? 'green' : 'red'"
+      :icon="alertType === 'success' ? 'mdi-check-circle' : 'mdi-alert-circle'"
+      :elevation="2"
+    >
       {{ alertMessage }}
     </v-alert>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { login } from '@/api/auth';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { login } from "@/api/auth";
 
 const router = useRouter();
-const usuario = ref<string>('');
-const senha = ref<string>('');
+const usuario = ref<string>("");
+const senha = ref<string>("");
 const alert = ref<boolean>(false);
-const alertMessage = ref<string>('');
+const alertMessage = ref<string>("");
+const alertType = ref<"error" | "success" | "info" | "warning" | undefined>(
+  "error"
+);
 const showPassword = ref<boolean>(false);
+const loading = ref<boolean>(false);
 
 const handleLogin = async () => {
-  if (usuario.value === '' && senha.value === '') {
-    alertMessage.value = 'Por favor, preencha o usuário e a senha.';
+  if (usuario.value === "" && senha.value === "") {
+    alertMessage.value = "Por favor, preencha o usuário e a senha.";
+    alertType.value = "error";
     showAlert();
     return;
-  } else if (usuario.value === '') {
-    alertMessage.value = 'Por favor, preencha o usuário.';
+  } else if (usuario.value === "") {
+    alertMessage.value = "Por favor, preencha o usuário.";
+    alertType.value = "error";
     showAlert();
     return;
-  } else if (senha.value === '') {
-    alertMessage.value = 'Por favor, preencha a senha.';
+  } else if (senha.value === "") {
+    alertMessage.value = "Por favor, preencha a senha.";
+    alertType.value = "error";
     showAlert();
     return;
   }
 
+  loading.value = true;
   try {
-    const data = await login(usuario.value, senha.value); 
-    console.log('Login bem-sucedido:', data);
-    router.push('/home'); 
+    loading.value = true;
+    const data = await login(usuario.value, senha.value);
+    alertMessage.value = "Login realizado com sucesso!";
+    alertType.value = "success";
+    showAlert();
+    setTimeout(() => {
+      router.push("/home");
+    });
   } catch (error) {
     alertMessage.value = String(error);
+    alertType.value = "error";
     showAlert();
+  } finally {
+    loading.value = false;
   }
 };
 
