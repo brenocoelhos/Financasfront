e8c5de1a60e8eb26b9b7ad237a6b3dbab37201d1
AdrianResende <adrianresende147@gmail.com>
2025-04-09 14:36:37 -0300

    fix: add configure to token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // URL base do backend
-  timeout: 5000, // Tempo limite para requisições
+  baseURL: 'http://localhost:8080'
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
